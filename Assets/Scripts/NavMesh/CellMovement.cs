3d5b04e9c22ab25c34ea672a598d95a4b06a7bef
Thomas-Johansen <thomassandjoh@hotmail.com>
2025-02-11 04:42:38 +0100

    I understand now...... But yea, need to fix how cells find their location so that it adapts to rotational changes. Alternatively we just say "f-it" and place each destination manualy

--- Assets/Scripts/NavMesh/CellMovement.cs
+++ Assets/Scripts/NavMesh/CellMovement.cs
@@ -6,6 +6,10 @@ public class CellMovement : MonoBehaviour
     public NavMeshAgent agent;
     public Transform target;
     
+   public Transform centerPoint;
+   public Transform edgePoint;
+   public float innerRadi;
+    
     bool go = false;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,10 +24,37 @@ public class CellMovement : MonoBehaviour
         if (go == false)
         {
             go = true;
-            Vector3 targetPos = target.position;
-            Vector3 avalidNavMeshPos;
+            //Vector3 targetPos = target.position;
+            Vector3 targetPos = GetRandomNavMeshPos();
+            
+            
             
             agent.SetDestination(targetPos);
         }
+    }
+
+    Vector3 GetRandomNavMeshPos()
+    {
+        innerRadi = 0.9f;
+        Vector3 center = centerPoint.position; 
+        float maxRadius = (center - edgePoint.position).magnitude;
+        float minRadius = maxRadius * innerRadi;
+        
+        for (int i = 0; i < 100; i++) // Try 10 times to find a valid point
+        {
+            Vector3 randomDir = Random.insideUnitCircle.normalized;
+            Quaternion rotation = Quaternion.Euler(90, 0, 0);
+            randomDir = rotation * randomDir;
+            float randomRadius = Mathf.Sqrt(Random.Range(minRadius * minRadius, maxRadius * maxRadius));
+            Vector3 randomPos = center + randomDir * randomRadius;
+            
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPos, out hit, maxRadius, NavMesh.AllAreas))
+            {
+                return hit.position; // Found a valid point on the NavMesh
+            }
+        }
+
+        return center;
     }
 }
