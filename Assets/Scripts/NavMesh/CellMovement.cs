53c3c899bdaa504da39431a50d0395f455c55ab7
Thomas-Johansen <thomassandjoh@hotmail.com>
2025-02-12 02:58:58 +0100

    Test using Cell models instead of Sphere

--- Assets/Scripts/NavMesh/CellMovement.cs
+++ Assets/Scripts/NavMesh/CellMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine.AI;
 public class CellMovement : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public Transform target;
     
+
+    public Transform parentLocation;
    public Transform centerPoint;
    public Transform edgePoint;
    public float innerRadi;
@@ -35,18 +36,22 @@ public class CellMovement : MonoBehaviour
 
     Vector3 GetRandomNavMeshPos()
     {
-        innerRadi = 0.9f;
+        innerRadi = 0.8f;
         Vector3 center = centerPoint.position; 
         float maxRadius = (center - edgePoint.position).magnitude;
         float minRadius = maxRadius * innerRadi;
+        print("Parent: " + parentLocation.rotation.eulerAngles.ToString());
         
         for (int i = 0; i < 100; i++) // Try 10 times to find a valid point
         {
             Vector3 randomDir = Random.insideUnitCircle.normalized;
-            Quaternion rotation = Quaternion.Euler(90, 0, 0);
+            Quaternion rotation = Quaternion.Euler(parentLocation.rotation.eulerAngles.x + 90, parentLocation.rotation.eulerAngles.y, parentLocation.rotation.eulerAngles.z);
+            print("Calculated: " + rotation.ToString());
+            //Quaternion rotation = Quaternion.Euler(90, 0, 0);
             randomDir = rotation * randomDir;
             float randomRadius = Mathf.Sqrt(Random.Range(minRadius * minRadius, maxRadius * maxRadius));
             Vector3 randomPos = center + randomDir * randomRadius;
+            
             
             NavMeshHit hit;
             if (NavMesh.SamplePosition(randomPos, out hit, maxRadius, NavMesh.AllAreas))
