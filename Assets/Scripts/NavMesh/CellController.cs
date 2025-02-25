459cc528bccf94083e4dabc60cfecf0313460e98
Thomas-Johansen <thomassandjoh@hotmail.com>
2025-02-25 05:45:34 +0100

    Add CellController and integrate SharedTimingData script
    Introduced a new CellController GameObject with associated components for controlling cells on the NavMesh. Enhanced the SharedTimingData script to enable shared animation state management and updated animations to include new stages.
    
    Only stage 1 is integrated as of yet

--- Assets/Scripts/NavMesh/CellController.cs
+++ Assets/Scripts/NavMesh/CellController.cs
@@ -2,11 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/// <summary>
+/// This Scripts is the main controller for spawning and controlling the cells on the navmesh
+/// </summary>
 public class CellController : MonoBehaviour
 {
-    /// <summary>
-    /// This Scripts is the main controller for spawning and controlling the cells on the navmesh
-    /// </summary>
+    
     
     //Cell variables
     
@@ -26,6 +27,20 @@ public class CellController : MonoBehaviour
     public Transform edgePoint;
     public float innerRadi;
     
+    //Timing variables
+    public SharedTimingData sharedTimingData;
+    /*
+     * Stages of animation:
+     *  Stage1: The muscle layer contracts
+     *  Stage2: Some cells starts to show up.
+     *  Stage3: The tissue gets irritated and contracts, slime starts showing up
+     *
+     * Stage 4: Pause for explanation or similar mid animation
+     *
+     * Stage 5: Medicine is administered
+     * Stage 6: Quick acting medicine makes the muscle layer expand/return to original size.
+     * Stage 7: Cells go away over time and tissue layer calms down.
+     */
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/SharedTimingData.cs
+++ Assets/Scripts/SharedTimingData.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
-
+/// <summary>
+/// This Scriptable object acts as a data sharing point between the Cell and Crossection shader controllers. \n
+/// It Contains variables for: \n
+///  - Animation speed.
+///  - Time elaspsed in the animation.
+///  - Stage of the animation
+/// </summary>
 [CreateAssetMenu(fileName = "SharedTimingData", menuName = "Scriptable Objects/SharedTimingData")]
 public class SharedTimingData : ScriptableObject
 {
@@ -12,9 +18,28 @@ public class SharedTimingData : ScriptableObject
     /// <summary>
     /// The elapsed time used for timing mechanisms.
     /// </summary>
-    public float time; 
+    public float time = 0; 
     /// <summary>
     /// Which stage of the animation we are at.
     /// </summary>
-    public int stage;
+    public int stage = 1;
+    /*
+     * Stages of animation:
+     *  Stage1: The muscle layer contracts
+     *  Stage2: Some cells starts to show up.
+     *  Stage3: The tissue gets irritated and contracts, slime starts showing up
+     *
+     * Stage 4: Pause for explanation or similar mid animation
+     *
+     * Stage 5: Medicine is administered
+     * Stage 6: Quick acting medicine makes the muscle layer expand/return to original size.
+     * Stage 7: Cells go away over time and tissue layer calms down.
+     */
+
+    void OnEnable()
+    {
+        speed = 1.0f;
+        time = 0.0f;
+        stage = 1; //Set to 0 here so we can start the animation at a spesific point from somewhere else
+    }
 }
